86135376d8a69b1a6e11c1c4a06674d8606af7e4
Guillerming <hello@guillerming.es>
2021-04-16 03:08:23 +0200

    Second iteration

--- index.php
+++ index.php
@@ -92,9 +92,9 @@
         <meta charset="utf-8" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,700;0,900&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="/assets/css/bootstrap/bootstrap.css" />
-        <link rel="stylesheet" href="/assets/css/styles.css" />
-        <link rel="stylesheet" href="/assets/css/sidebar.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
+        <link rel="stylesheet" type="text/css" href="/assets/css/styles.css" />
+        <link rel="stylesheet" type="text/css" href="/assets/css/sidebar.css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     </head>
 
@@ -251,7 +251,7 @@
 
         </div>
 
-        <script src="/assets/js/bootstrap/bootstrap.bundle.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         <script src="/assets/js/form.js"></script>
     </body>
 
